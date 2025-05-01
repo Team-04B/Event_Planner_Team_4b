fd89f160ca5a73469c796e8b923ade282d2d9ee1
Amdadul-HQ <rimonamdadul301@gmail.com>
2025-05-01 15:31:26 +0600

    add: all the routes in api

--- backend/src/app/routers/index.ts
+++ backend/src/app/routers/index.ts
@@ -1,12 +1,51 @@
 import express from 'express';
+import { AuthRoutes } from '../../modules/auth/auth.routes';
+import { UsersRoutes } from '../../modules/users/users.routes';
+import { EventRoutes } from '../../modules/events/event.routes';
+import { ParticipationRoutes } from '../../modules/participation/participation.routes';
+import { InvitationRoutes } from '../../modules/invitations/invitations.routes';
+import { PaymentRoutes } from '../../modules/payments/payments.routes';
+import { ReviewsRoutes } from '../../modules/reviews/reviews.routes';
+import { AdminRoutes } from '../../modules/admin/admin.routes';
 
 const router = express.Router();
 
 const moduleRoutes = [
-
+    {
+        path:'/auth',
+        route:AuthRoutes
+    },
+    {
+        path:'/users',
+        route:UsersRoutes
+    },
+    {
+        path:'/events',
+        route:EventRoutes
+    },
+    {
+        path:'/participants',
+        route:ParticipationRoutes
+    },
+    {
+        path:'/invitations',
+        route:InvitationRoutes
+    },
+    {
+        path:'/payments',
+        route:PaymentRoutes
+    },
+    {
+        path:'/reviews',
+        route:ReviewsRoutes
+    },
+    {
+        path:'/admin',
+        route:AdminRoutes
+    }
 ]
 
-// moduleRoutes.forEach(route => router.use(route.path,route.route));
+moduleRoutes.forEach(route => router.use(route.path,route.route));
 
 
 export default router;
