c693229c846ce3122be8f15cdcf54d00651bf4e4
SanimSRk <hasansanim562@gmail.com>
2025-05-02 22:26:41 +0600

    user is done

--- backend/src/modules/users/users.routes.ts
+++ backend/src/modules/users/users.routes.ts
@@ -1,5 +1,16 @@
-import express from "express"
+import express from 'express';
+import { UserContolors } from './users.controller';
+import auth from '../../app/middleWares/auth';
+import { Role } from '@prisma/client';
 
-const router = express.Router()
+const router = express.Router();
 
-export const UsersRoutes = router
+router.get('/', auth(Role.ADMIN), UserContolors.getAllUsers);
+router.get('/:id', auth(Role.ADMIN, Role.USER), UserContolors.getSingleUsers);
+router.put(
+  '/:id',
+  auth(Role.ADMIN, Role.USER),
+  UserContolors.updateSingleUsers
+);
+router.delete('/:id', auth(Role.ADMIN), UserContolors.deletedSingleUsers);
+export const UsersRoutes = router;
