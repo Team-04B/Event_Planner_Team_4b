75f24ada7f9d3300431393c6d1f4846c83c21563
SanimSRk <hasansanim562@gmail.com>
2025-05-04 12:33:09 +0600

    login and sing up ui design is done

--- frontend/src/components/modules/register/register.tsx
+++ frontend/src/components/modules/register/register.tsx
@@ -12,7 +12,7 @@ import {
 import { Input } from "@/components/ui/input";
 import Link from "next/link";
 
-const LoginPage = () => {
+const RegisterPage = () => {
   const form = useForm();
   const {
     formState: { isSubmitting },
@@ -24,7 +24,7 @@ const LoginPage = () => {
     <div className="border-2 border-gray-300 rounded-xl flex-grow max-w-md w-full p-5">
       <div className="flex items-center  space-x-4 mb-6 ">
         <div className="">
-          <h1 className="text-xl font-semibold">Login</h1>
+          <h1 className="text-xl font-semibold">Register</h1>
           <p className="font-extralight text-sm text-gray-600">Welcome back!</p>
         </div>
       </div>
@@ -32,17 +32,32 @@ const LoginPage = () => {
         <form onSubmit={form.handleSubmit(onSubmit)}>
           <FormField
             control={form.control}
-            name="email"
+            name="name"
             render={({ field }) => (
               <FormItem>
-                <FormLabel>Email</FormLabel>
+                <FormLabel>Full Name</FormLabel>
                 <FormControl>
-                  <Input type="email" {...field} value={field.value || ""} />
+                  <Input type="text" {...field} value={field.value || ""} />
                 </FormControl>
                 <FormMessage />
               </FormItem>
             )}
           />
+          <div className="mt-4">
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input type="email" {...field} value={field.value || ""} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
           <div className="mt-4">
             <FormField
               control={form.control}
@@ -63,23 +78,19 @@ const LoginPage = () => {
             />
           </div>
 
-          <Button
-            // disabled={reCaptchaStatus ? false : true}
-            type="submit"
-            className="mt-5 cursor-pointer w-full"
-          >
-            {isSubmitting ? "Logging...." : "Login"}
+          <Button type="submit" className="mt-5 cursor-pointer w-full">
+            {isSubmitting ? "Register...." : "Register Now"}
           </Button>
         </form>
       </Form>
       <p className="text-sm text-gray-600 text-center my-3">
-        Do not have any account ?
-        <Link href="/register" className="text-blue-500">
-          Register
+        Already have a account ?
+        <Link href="/login" className="text-blue-500">
+          Login
         </Link>
       </p>
     </div>
   );
 };
 
-export default LoginPage;
+export default RegisterPage;
