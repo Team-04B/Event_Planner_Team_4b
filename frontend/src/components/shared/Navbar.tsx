1d68ba6d360289c44ca9a0fb4ca3aacc64cb60e2
unknown <mydulcse62.niter@gmail.com>
2025-05-05 19:24:20 +0600

    Navbar-hero:navbar hero section create

--- frontend/src/components/shared/Navbar.tsx
+++ frontend/src/components/shared/Navbar.tsx
@@ -57,11 +57,7 @@ export default function Navbar() {
   }
 
   const markAllAsRead = () => {
-    setNotifications(notifications.map((n) => ({ ...n, read: true })))
-    toast({
-      title: "All notifications marked as read",
-      description: "All notifications have been marked as read",
-    })
+  
   }
 
   const handleAcceptInvitation = (id: string, eventId: string) => {}
@@ -193,7 +189,7 @@ function NotificationsDesktop({
                   <div className="flex gap-2 mt-2">
                     <Button
                       size="sm"
-                      className="h-8 w-full"
+                      className="h-8 "
                       onClick={() => handleAcceptInvitation(notification.id, notification.eventId!)}
                     >
                       Accept
@@ -201,7 +197,7 @@ function NotificationsDesktop({
                     <Button
                       size="sm"
                       variant="outline"
-                      className="h-8 w-full"
+                      className="h-8 "
                       onClick={() => handleDeclineInvitation(notification.id, notification.eventId!)}
                     >
                       Decline
@@ -269,7 +265,7 @@ function NotificationsMobile({
                   <div className="flex gap-2 mt-2">
                     <Button
                       size="sm"
-                      className="h-8 w-full"
+                      className="h-8 "
                       onClick={() => handleAcceptInvitation(notification.id, notification.eventId!)}
                     >
                       Accept
@@ -277,7 +273,7 @@ function NotificationsMobile({
                     <Button
                       size="sm"
                       variant="outline"
-                      className="h-8 w-full"
+                      className="h-8 "
                       onClick={() => handleDeclineInvitation(notification.id, notification.eventId!)}
                     >
                       Decline
