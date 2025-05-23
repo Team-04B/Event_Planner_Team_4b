00ec86c294b134079d348f9e24e2f1162fb28343
Amdadul-HQ <rimonamdadul301@gmail.com>
2025-05-23 19:03:27 +0600

    add: register ui change

--- frontend/src/app/register/page.tsx
+++ frontend/src/app/register/page.tsx
@@ -3,7 +3,7 @@ import RegisterPage from "@/components/modules/register/register";
 const Register = () => {
   return (
     <div className="flex justify-center items-center  min-h-screen">
-      <RegisterPage></RegisterPage>
+      <RegisterPage/>
     </div>
   );
 };
